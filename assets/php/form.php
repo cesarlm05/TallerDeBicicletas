4cc81a32caf66cee77b848e5c5e9f2cc2e65b6db
cesarlm05 <cesarlm05@gmail.com>
2022-05-16 19:55:20 -0300

    Update

--- assets/php/form.php
+++ assets/php/form.php
@@ -14,16 +14,16 @@ $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['email'];
 //$socio = $_POST['form__option'];
-$mensaje = $_POST['textarea'];
+$mensaje_body = $_POST['textarea'];
 
 // Headers texto/html
-$header.= "content-type: text/html\n";
+$header.= "content-type: html\n";
 
 // Cuerpo del mensaje
 $mensaje = "Este mensaje fue enviado por: " . $nombre . "\nApellido: " . $apellido . ",\r\n";
 $mensaje .=  "Email: " . $email . ",\r\n";
 //$mensaje .= "Es socio/a: " . $socio . ",\r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . ",\r\n";
+$mensaje .= "Mensaje: " . $mensaje_body . ",\r\n";
 $mensaje .= "Enviado el: " . date('d/m/Y', time());
 // Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
 $mensaje = wordwrap($mensaje, 70, "\r\n");
@@ -35,6 +35,6 @@ $subject = 'Mensaje desde el sitio web de Ojo Clinico Bike';
 // Enviar el mensaje
 mail($mailto, $subject, utf8_decode($mensaje), $header);
 
-header('Location:../index.html');
+header('Location:index.html');
 
 ?>
