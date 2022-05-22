32678090c22af185599cf053e50c0015f3e931d6
cesarlm05 <cesarlm05@gmail.com>
2022-05-22 12:09:12 -0300

    Add Form-php

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,40 +1,14 @@
 <?php
-//==============================================================================
-//$name = "Da Duder"; //senders name
-//$email = "email@adress.com"; //senders e-mail adress
-//$recipient = "PersonWhoGetsIt@emailadress.com"; //recipient
-//$mail_body = "The text for the mail..."; //mail body
-//$subject = "Subject for reviever"; //subject
-//$header = "From: ". $name . " <" . $email . ">\r\n"; //optional headerfields
-//
-//mail($recipient, $subject, $mail_body, $header); //mail command :)
-//==============================================================================
+
+$destino = "ojo.clinico.bike@gmail.com";
 
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['email'];
-//$socio = $_POST['form__option'];
-$mensaje_body = $_POST['textarea'];
+$mensaje_body = $_POST['textbody'];
+$asunto = "Mensaje desde el sitio web de Ojo Clinico Bike";
 
-// Headers texto/html
-$header.= "content-type: html\n";
-
-// Cuerpo del mensaje
-$mensaje = "Este mensaje fue enviado por: " . $nombre . "\nApellido: " . $apellido . ",\r\n";
-$mensaje .=  "Email: " . $email . ",\r\n";
-//$mensaje .= "Es socio/a: " . $socio . ",\r\n";
-$mensaje .= "Mensaje: " . $mensaje_body . ",\r\n";
-$mensaje .= "Enviado el: " . date('d/m/Y', time());
-// Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
-$mensaje = wordwrap($mensaje, 70, "\r\n");
-
-
-$mailto = 'ojo.clinico.bike@gmail.com';
-$subject = 'Mensaje desde el sitio web de Ojo Clinico Bike';
-
-// Enviar el mensaje
-mail($mailto, $subject, utf8_decode($mensaje), $header);
-
-header('Location:index.html');
-
-?>
+$header = "Mensaje desde el sitio web de Ojo Clinico Bike contacto en localhost";
+$mensaje = "\n Nombre: " . $nombre . "\n Apellido: " . $apellido . "\n" . "Email: " . $email . "\n" . "Mensaje :" . $mensaje_body;
+mail($destino, $asunto, $mensaje, $header);
+header('Location: index.html');
